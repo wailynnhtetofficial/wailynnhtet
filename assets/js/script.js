7f246f9f8d5846c432b924cff9af98699b2d1bb0
Wai Lynn Htet <57894955+wailynnhtetofficial@users.noreply.github.com>
2023-01-12 12:04:19 +0630

    Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Jigar Sable";
+            document.title = "Wai Lynn Htet";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["Frontend Development", "Backend Development", "Web Designing", "Android Development", "Web Development"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
